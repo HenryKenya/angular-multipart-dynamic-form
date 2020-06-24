0d7df4e96845620741f0ac9236a5799244ae2984
henry <dru@Henrys-MacBook-Pro.local>
2020-06-24 16:31:45 +0300

    basic styling load component with data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "./data.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'multipart-form';
+export class AppComponent implements OnInit {
+  users: any[] = [];
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getDataFromApi().subscribe(
+      (data) => {
+        this.users = data;
+      },
+      (error) => console.log(error)
+    );
+  }
 }
